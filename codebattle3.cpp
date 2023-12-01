99e8c0ff7b28e9097b24711cd2e09fd7688cfdbd
sangbumlikeagod <oironese@naver.com>
2023-12-01 23:04:21 +0900

    commit codebattle3 -ing

--- codebattle3.cpp
+++ codebattle3.cpp
@@ -3,21 +3,67 @@
 #include <vector>
 #include <queue>
 using namespace std;
+const int NODE_MAX = 5000;
+
+struct Node{
+    Node(int a, Node* x) : val(a), next(x){};
+    Node(int a){
+        val = a;
+        next = nullptr;
+    }
+    int val;
+    Node* next;
+
+};
+
+class LinkedList {
+    vector<Node*> nodeArr;
+    int nodeCnt;
+
+public:
+    Node* head;
+    Node* tail;
+    LinkedList() : head(nullptr), tail(nullptr), nodeCnt(0) {
+        nodeArr.resize(NODE_MAX, nullptr);
+    }
+    void addNode(Node* node){
+        if (head == nullptr)
+        {
+            head = node;
+            tail = node;
+        } 
+        else
+        {
+            tail->next = node;  
+            tail = node; 
+        }
+    }
+
+};
+
 
 
 int main(){
     ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
     ifstream cin("codebattle3.txt", ios_base::in);    
     
-    int testcase;
-    cin >> testcase;
     int N;
-    int res;
-    for (int tc = 1; tc <= testcase; tc++){
-        cin >> N;
-        res = 1;
-        cout << '#' << tc << ' ' << N * (res - 1) << '\n';
-    }
+    cin >> N;
+
+    // int* ptr_a = &a;
+    // 포인터로써 선언함
+
+    cout << (Node(5).next == nullptr) << '\n';
+    Node a = Node(5);
+    Node* ptr_a = &a;
+
+    Node b = Node(5, &a);
+    LinkedList list = LinkedList();
+    list.addNode(&a);
+    Node c = Node(3);
+    list.addNode(&c);
+    cout << list.head->val << ' ' << list.head->next->val;
+
 
 
 
