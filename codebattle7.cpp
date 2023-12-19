6c0b44bb1090255837e4aa38a843e91c43c91992
sangbumlikeagod <oironese@naver.com>
2023-12-19 22:56:44 +0900

    commit

---
+++ codebattle7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+
+
+int main(){
+    ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
+    ifstream cin(".txt", ios_base::in);    
+    int tc;
+    cin >> tc;
+    for (int t = 0; t < tc; t++){
+        int res;
+
+
+
+        cout << '#' << t + 1 << ' ' << res;
+    }
+}
