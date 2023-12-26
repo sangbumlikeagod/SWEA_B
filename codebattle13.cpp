451701cdaf2068344286e06a38adb4509dc073db
sangbumlikeagod <oironese@naver.com>
2023-12-26 14:13:40 +0900

    commit codebattle13

---
+++ codebattle13.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int dp[1001][1001];
+
+
+int main(){
+    ios::sync_with_stdio(false), cin.tie(0), cin.tie(0);
+    ifstream cin("codebattle13.txt", ios_base::in);    
+    int tc;
+    cin >> tc;
+    for (int t = 0; t < tc; t++){
+        for (int i = 0; i < 1001; i++){
+            memset(dp[i], 0, sizeof(dp[i]));
+        }
+        int res;
+        string s1;
+        string s2;
+        cin >> s1 >> s2;
+        if (s2.length() > s1.length()) swap(s1, s2);
+        int m = s1.length();
+        int n = s2.length();
+
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (s1[i] == s2[j]){
+                    dp[i][j]++;
+                }
+                dp[i + 1][j] = max(dp[i][j], dp[i + 1][j]);
+                dp[i][j + 1] = max(dp[i][j], dp[i][j + 1]);
+                dp[i + 1][j + 1] = max(dp[i][j], dp[i + 1][j + 1]);
+            }
+        }
+
+
+        // for (int i = 0; i <= m; i++)
+        // {
+        //     for (int k = 0; k <= n; k++)
+        //     {
+        //         cout << dp[i][k] << ' ';
+        //     }
+        //     cout << '\n';   
+        // }
+        
+        // for (int i = 0; i <= m; i++){
+        //     res = max(res, dp[i][n]);
+        // }
+        cout << '#' << t + 1 << ' ' << dp[m][n] << '\n';
+    }
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,12 @@
-                // cout << ans << '\t' << mAns << '\n';
+        for (int i = 0; i <= m; i++)
+        {
+            for (int k = 0; k <= n; k++)
+            {
+                cout << dp[i][k] << ' ';
+            }
+            cout << '\n';   
+        }
+        
+        for (int i = 0; i <= m; i++){
+            res = max(res, dp[i][n]);
+        }
