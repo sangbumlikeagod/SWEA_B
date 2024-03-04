07b795fb4d58303aae94b03d75298da63e1a2c02
sangbumlikeagod <oironese@naver.com>
2024-03-05 07:26:26 +0900

    feat : codebattle0004

---
+++ codebattle0004/solution.cpp
@@ -0,0 +1,17 @@
+void init(int H, int W, char mStr[])
+{
+}
+
+void insert(char mChar)
+{
+}
+
+char moveCursor(int mRow, int mCol)
+{
+    return '$';
+}
+
+int countCharacter(char mChar)
+{
+    return -1;
+}
